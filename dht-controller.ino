71ff481d7038e6582cd50eca32715943013e3b13
moonorongo <mscifu@gmail.com>
2017-04-08 11:20:05 -0300

    cambio de LCD a 7Seg, mejoras en el rango y en el codigo

--- dht-controller.ino
+++ dht-controller.ino
@@ -1,64 +1,125 @@
-#include <LiquidCrystal.h>
+#include <SevenSeg.h>
+SevenSeg sevseg(11,10,9,8,7,6,5);
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const int numOfDigits=2;
+int digitPins[numOfDigits]={4,3};
+
 #define tempRefPin A0  
 #define humRefPin A1  
-#define tempRelePin 6
-#define humRelePin 7
+#define tempRelePin 12
+#define humRelePin 13
 #define PAUSADHT 2000
+
+#define dummyTempPin A2  
+#define showSwitch A3
 
 int tempRefValue = 0; 
 int humRefValue = 0; 
 int tempRefLowValue = 0; 
 int humRefLowValue = 0; 
+int prevTempRefValue = tempRefValue;
+int prevHumRefValue = humRefValue;
 
 
 int hum = 70;
 int temp = 23;
+int changeTimer = 0;
 unsigned long dhtDelay;
 
 void setup() {
-  lcd.begin(16, 2);
+  sevseg.setDigitPins(numOfDigits ,digitPins);
+  sevseg.setCommonCathode();
+  sevseg.write(temp);
+  
   dhtDelay = millis();
+  pinMode(tempRelePin, OUTPUT);
+  pinMode(humRelePin, OUTPUT);
+  digitalWrite(tempRelePin, LOW);
+  digitalWrite(humRelePin, LOW);
 }
 
-// 0123456789ABCDFG
-// T:23 R: 28
-// H:70 R: 80
+void updateDisplay() {
+//    if (esta_seleccionado mostrar temperatura) {
+        if(changeTimer > 0) { // mostrar tempRefValue
+            sevseg.write(tempRefValue);
+        } else { // mostrar temp
+            sevseg.write(temp);
+        }
+//    } else { // mostrar humedad
+        
+//    }
+  
+}
 
-void updateLcd() {
-  lcd.setCursor(0, 0);
-  lcd.print("T: ");
-  lcd.print(temp);
 
-  lcd.setCursor(8, 0);
-  lcd.print(" M: ");
-  lcd.print(tempRefValue);
+
+void checkInputValues() {
+    if (temp >= tempRefValue) {
+        digitalWrite(tempRelePin, LOW);
+    }
+
+    if (temp <= tempRefLowValue) {
+        digitalWrite(tempRelePin, HIGH);
+    }
+
+    if (hum >= humRefValue) {
+        digitalWrite(humRelePin, LOW);
+    }
+
+    if (hum <= humRefLowValue) {
+        digitalWrite(humRelePin, HIGH);
+    }
+
+}
+
+void readRefValues() {
+  tempRefValue = map(analogRead(tempRefPin), 0, 1023, 15, 35);
+  humRefValue = map(analogRead(humRefPin), 0, 1023, 30, 99);
+  tempRefLowValue = tempRefValue - 2; 
+  humRefLowValue = humRefValue - 10; 
+
+  if (prevTempRefValue != tempRefValue) {
+    changeTimer = 50; 
+  }
+
+  if (prevHumRefValue != humRefValue) {
+    changeTimer = 50; 
+  }
+
+  prevTempRefValue = tempRefValue;
+  prevHumRefValue = humRefValue;
+}
+
+
+void readDHT() {
+// dummy for testing
+    temp = map(analogRead(dummyTempPin), 0, 1023, 15, 35);
     
-  lcd.setCursor(0, 1);
-  lcd.print("H: ");
-  lcd.print(hum);
-  lcd.print(" ");
-
-  lcd.setCursor(8, 1);
-  lcd.print(" M: ");
-  lcd.print(humRefValue);
-  lcd.print(" ");
-}
-
-void loop() {
+/*    
   if (millis() > dhtDelay + PAUSADHT) {
     dhtDelay = millis();
     // leer con el dht  
       // leo hum
       // leo temp
   }
+*/  
+}
 
-  tempRefValue = map(analogRead(tempRefPin), 0, 1023, 15, 35);
-  humRefValue = map(analogRead(humRefPin), 0, 1023, 30, 100);
-  
-  updateLcd();
+/*
+showSwitch: implementar que switchee entre 2 estados, para mostrar/editar temperatura/humedad 
 
+*/
+
+void loop() {
+  readDHT();
+  readRefValues();
+  checkInputValues();
+ 
+  updateDisplay();
+
+  if (changeTimer > 0) {
+    changeTimer--;
+  }
   
 }
 
